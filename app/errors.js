81783a3b4e19125f49d97b02d59812776a8fd6b4
Leandro Di Paola <leandro.dipaola@wolox.com.ar>
2020-11-26 15:16:21 -0300

    added user models

--- app/errors.js
+++ app/errors.js
@@ -11,3 +11,12 @@ exports.defaultError = message => internalError(message, exports.DEFAULT_ERROR);
 
 exports.API_JOKES_ERROR = 'api_jokes_error';
 exports.apiJokesError = message => internalError(message, exports.API_JOKES_ERROR);
+
+exports.USER_SIGNUP_ERROR = 'user_signup_error';
+exports.userSignupError = message => internalError(message, exports.USER_SIGNUP_ERROR);
+
+exports.ENCRYPT_PASSWORD_ERROR = 'encrypt_password_error';
+exports.encryptError = message => internalError(message, exports.USER_SIGNUP_ERROR);
+
+exports.INVALID_PASSWORD_ERROR = 'invalid_password_error';
+exports.invalidPasswordError = message => internalError(message, exports.INVALID_PASSWORD_ERROR);
